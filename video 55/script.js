9f07c0b149a6e109c8079d331d7c5fbee65af634
ruhii-verma <ruhi9401verma@gmail.com>
2024-05-24 13:09:19 +0530

    initail commit

---
+++ video 55/script.js
@@ -0,0 +1,31 @@
+// var a = 4;
+// var b = 5;
+// var c = "rashi";
+
+// console.log(a + b);
+// console.log(typeof(a), typeof(b),typeof(c));
+// const a1 = 6;
+// a1 = a1+1; //not allowed because a1 is constant
+// var a = 5;
+// {
+//     var a = 66;
+//     console.log(a);
+// } 
+
+let a = "rashi";
+let b = 22;
+let c = 2.22;
+const d = true;
+let r = null;
+console.log(a,b,c,d,r);
+console.log(typeof a,typeof b,typeof c ,typeof d,typeof r);
+
+let o = {
+    "name" :"harry",
+    "job code": 5600
+}
+console.log(o)
+o.salary = "10 lakh";
+console.log(o)
+o.salary = "1 lakh";
+console.log(o)
